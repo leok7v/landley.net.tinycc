49604b2127804e87a728554fad1924d77341b649
Rob Landley <rob@landley.net>
2007-05-16 20:04:21 -0400

    Patch from Zdenek Pavlas, affects PE target: > Because of Windows' draconic handling of page faults 'sub esp, 4096' is > a no-no.  Rare functions needing page or more of stack space must call > chkstk(stacksize) first.  TCC needs 10 bytes for such call while the > standard function prolog was only 9 so it pads them to 10 bytes. > > This patch uses a shorter chkstk() calling convention that fits in 9 > bytes so no more padding.

--- i386/i386-gen.c
+++ i386/i386-gen.c
@@ -407,11 +407,7 @@ void gfunc_call(int nb_args)
     vtop--;
 }
 
-#ifdef TCC_TARGET_PE
-#define FUNC_PROLOG_SIZE 10
-#else
 #define FUNC_PROLOG_SIZE 9
-#endif
 
 /* generate function prolog of type 't' */
 void gfunc_prolog(CType *func_type)
@@ -541,7 +537,6 @@ void gfunc_epilog(void)
 #ifdef TCC_TARGET_PE
     if (v >= 4096) {
         Sym *sym = external_global_sym(TOK___chkstk, &func_old_type, 0);
-        oad(0xb8, v); /* mov stacksize, %eax */
         oad(0xe8, -4); /* call __chkstk, (does the stackframe too) */
         greloc(cur_text_section, sym, ind-4, R_386_PC32);
     } else
@@ -549,11 +544,8 @@ void gfunc_epilog(void)
     {
         o(0xe58955);  /* push %ebp, mov %esp, %ebp */
         o(0xec81);  /* sub esp, stacksize */
-        gen_le32(v);
-#if FUNC_PROLOG_SIZE == 10
-        o(0x90);  /* adjust to FUNC_PROLOG_SIZE */
-#endif
     }
+    gen_le32(v);
     ind = saved_ind;
 }
 
