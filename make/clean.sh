16d427795396358c464ed500ac1c196ddc1673e5
Rob Landley <rob@landley.net>
2007-12-10 16:34:38 -0600

    Clean shouldn't try to remove the directory "tests".

--- make/clean.sh
+++ make/clean.sh
@@ -4,5 +4,5 @@ source configure
 
 # Need to figure out how much of this is needed...
 $DEBUG rm -f *~ *.o *.a tinycc *-tinycc *-tinycc_unstripped a.out \
-      test? test?.out \
+      test[0-9] test[0-9].out \
       libtcc_test tinycc.1 *.bin *.i ex2 core gmon.out *.exe *.lib win32/lib/*.o
