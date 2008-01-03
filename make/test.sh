1651f022d4bbf35e768a0e6683b50100784de58f
Rob Landley <rob@landley.net>
2008-01-03 17:10:59 -0600

    Upgrade test.sh to test tinycc as a shared library.

--- make/test.sh
+++ make/test.sh
@@ -2,9 +2,18 @@
 
 source ./configure
 
-$CC $CFLAGS -I. -Iinclude -o test1 tests/tcctest.c &&
-./test1 > test1.out &&
-./tinycc -I. -Iinclude -o test2 tests/tcctest.c &&
-./test2 > test2.out &&
-diff -u test1.out test2.out
-[ $? -eq 0 ] && echo Test passed.
+$DEBUG $CC $CFLAGS -I. -Iinclude -o test1 tests/tcctest.c &&
+$DEBUG ./test1 > test1.out &&
+$DEBUG ./tinycc -I. -L. -Iinclude -o test2 tests/tcctest.c &&
+$DEBUG ./test2 > test2.out &&
+$DEBUG diff -u test1.out test2.out
+if [ $? -eq 0 ]
+then
+  echo Tinycc test passed.
+else
+  exit 1
+fi
+
+$DEBUG $CC $CFLAGS -Iinclude -L . -ltinycc-i386 tests/libtcc_test.c -o libtcc_test &&
+LD_LIBRARY_PATH=. $DEBUG ./libtcc_test
+[ $? -eq 0 ] && echo libtinycc test passed.
