a2054cdb19f117a2569b282c2f20fc69387add32
Rob Landley <rob@landley.net>
2008-01-14 22:38:23 -0600

    Clean should delete shared libraries, too.

--- make/clean.sh
+++ make/clean.sh
@@ -3,6 +3,6 @@
 source configure
 
 # Need to figure out how much of this is needed...
-$DEBUG rm -f *~ *.o *.a tinycc *-tinycc *-tinycc_unstripped a.out \
+$DEBUG rm -f *~ *.o *.a *.so tinycc *-tinycc *-tinycc_unstripped a.out \
       test[0-9] test[0-9].out \
       libtcc_test tinycc.1 *.bin *.i ex2 core gmon.out *.exe *.lib win32/lib/*.o
